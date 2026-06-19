63c6a4a7f317943e320c0aa8dcbe022041cf5d7d
Musaib Naseem <musaibnaseem786@gmail.com>
2026-06-19 14:28:31 +0530

    Add draggable nodes and fix Vite Compatibility

--- src/Components/Node/Node.jsx
+++ src/Components/Node/Node.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import Draggable from "react-draggable";
 
-const Node=({id,label,x,y,deleteNode})=>{
+const Node = ({ id, label, x, y, deleteNode }) => {
+  return (
+    <Draggable>
+      <div
+        style={{
+          position: "absolute",
+          left: `${x}px`,
+          top: `${y}px`,
+          padding: "10px",
+          border: "1px solid black"
+        }}
+      >
+        <h2>{id} - {label}</h2>
 
-return(
-
-<div style={{ position:"absolute",left:`${x}px`, right:`${y}px`, padding:"10px"}}>
-
-<h2> {id} - { label } </h2>
-
-<button onClick={()=>deleteNode(id)}> Delete Node </button>
-
-</div>
-
-)
-
-}
+        <button onClick={() => deleteNode(id)}>
+          Delete Node
+        </button>
+      </div>
+    </Draggable>
+  );
+};
 
 export default Node;
